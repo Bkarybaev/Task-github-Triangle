dc52d22a231b20d54ca307dd303dfe965629729c
Bkarybaev <baielkarybaev@gmail.com>
2024-11-05 16:35:49 +0600

    first commit

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Triangle {
+    double a;
+    double b;
+    double c;
+
+    public double area() {
+        double res = (a + b + c) / 2;
+        return Math.sqrt(res * (res - a) * (res - b) * (res - c));
+    }
 }
